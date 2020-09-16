19c37a874bc64840b5f372c985718a570e5ce06e
Nguyễn Trọng Vĩnh <ntvinh.it@gmail.com>
2020-09-16 11:02:13 +0700

    Update attach position

--- attach.js
+++ attach.js
@@ -6,68 +6,61 @@ let timer;
 
 export default class Attach extends React.Component {
   render() {
-    const { center, name, children, style, viewStyle } = this.props;
+    const { center, name, children, style } = this.props;
     if (!name) return children;
 
     return (
       <Consumer>
         {({ onLayout, step }) => {
-          return step.name === name ? (
-            <View
-              ref={ref => {
-                this.marker = ref;
-              }}
-              style={viewStyle}
-              onLayout={() => {
-                // make sure we only render the tour
-                // after elements stop moving, ex route transition
+          if (step.name !== name) return children;
 
-                timer = setInterval(
-                  () =>
-                    this.marker &&
-                    this.marker.measure((x, y, width, height, pageX, pageY) => {
-                      if (
-                        this.x === x &&
-                        this.y === y &&
-                        this.pageX === pageX &&
-                        this.pageY === pageY
-                      )
-                        clearInterval(timer);
-                      else {
-                        this.x = x;
-                        this.y = y;
-                        this.pageX = pageX;
-                        this.pageY = pageY;
-                        return;
-                      }
+          const newChildren = React.cloneElement(children, {
+            onLayout: (event) => {
+              if (children.props.onLayout) children.props.onLayout(event);
 
-                      const circleSize = height > width ? height : width;
+              event.persist();
 
-                      onLayout(name, {
-                        center,
-                        style: {
-                          // make sure the highlight is always a circle
-                          height: circleSize,
-                          width: circleSize,
-                          top: pageY,
-                          left: pageX,
-                          borderRadius: circleSize,
-                          ...(style
-                            ? style({ height, width, pageX, pageY, x, y })
-                            : {}),
-                        },
-                        overlay: children,
-                      });
-                    }),
-                  250,
-                );
-              }}
-            >
-              {children}
-            </View>
-          ) : (
-            children
-          );
+              timer = setInterval(() => {
+                event.target &&
+                event.target.measure((x, y, width, height, pageX, pageY) => {
+                  if (
+                    this.x === x &&
+                    this.y === y &&
+                    this.pageX === pageX &&
+                    this.pageY === pageY
+                  )
+                    clearInterval(timer);
+                  else {
+                    this.x = x;
+                    this.y = y;
+                    this.pageX = pageX;
+                    this.pageY = pageY;
+                    return;
+                  }
+
+                  const circleSize = height > width ? height : width;
+
+                  onLayout(name, {
+                    center,
+                    style: {
+                      // make sure the highlight is always a circle
+                      height: circleSize,
+                      width: circleSize,
+                      top: pageY - ((circleSize - height) / 2),
+                      left: pageX - ((circleSize - width) / 2),
+                      borderRadius: circleSize,
+                      ...(style
+                        ? style({ height, width, pageX, pageY, x, y })
+                        : {}),
+                    },
+                    overlay: children,
+                  });
+                })
+              }, 250);
+            }
+          });
+
+          return newChildren;
         }}
       </Consumer>
     );
